8266ff54dabab1707887313ac2184dfb9d17f7a6
Marcin <marcin.juranek2@gmail.com>
2019-05-21 12:37:47 +0200

    Changing  first abstract class too AGraphicCanvas and changing name of AMainTools too ACoordinates

--- paint/PaintTools/AMainTools.cs
+++ paint/PaintTools/AMainTools.cs
@@ -8,13 +8,25 @@ using System.Windows.Forms;
 
 namespace paint.PaintTools
 {
-    public abstract class AMainTools
+
+    public abstract class AGraphicCanvas 
+    {
+        public Graphics graphic { get; private set; }
+
+        public AGraphicCanvas(PictureBox canvasControl)
+        {
+            this.graphic = canvasControl.CreateGraphics();
+        }
+    }
+
+    public abstract class ACoordinates : AGraphicCanvas
     {
         public Point pointStart { get; private set; }
         public Point pointEnd { get; private set; }
         public int size { get; set; }
 
-        public AMainTools(int size, int xEnd, int yEnd, int xStart, int yStart, ICoordinates icoordintes)
+        public ACoordinates(PictureBox canvasControl,int size, int xEnd, int yEnd, int xStart, int yStart, ICoordinates icoordintes)
+            :base(canvasControl)
         {
             this.pointEnd = icoordintes.setEndCoordinates(xEnd, yEnd);
             //defalut values for start coodinates are 0 (usually)
@@ -23,18 +35,7 @@ namespace paint.PaintTools
         }
     }
 
-    public abstract class AGraphicCanvas : AMainTools
-    {
-        public Graphics graphic { get; private set; }
-
-        public AGraphicCanvas(PictureBox canvasControl, int size, int xEnd, int yEnd, int xStart, int yStart, ICoordinates icoordintes)
-            : base(size, xEnd, yEnd, xStart, yStart, icoordintes)
-        {
-            this.graphic = canvasControl.CreateGraphics();
-        }
-    }
-
-    public abstract class APenToolType : AGraphicCanvas
+    public abstract class APenToolType : ACoordinates
     {
         public APenToolType(PictureBox canvasControl,int size, int xEnd, int yEnd, int xStart, int yStart, ICoordinates icoordintes) 
             : base(canvasControl,size, xEnd, yEnd, xStart, yStart, icoordintes)
@@ -44,7 +45,7 @@ namespace paint.PaintTools
         public abstract bool getMainToolTypePen(IPenTool ipen, Color color, string name);
     }
 
-    public abstract class ABrushToolType : AGraphicCanvas
+    public abstract class ABrushToolType : ACoordinates
     {
         public ABrushToolType(PictureBox canvasControl,int size, int xEnd, int yEnd, int xStart, int yStart, ICoordinates icoordintes)
             : base(canvasControl, size, xEnd, yEnd, xStart, yStart, icoordintes)
